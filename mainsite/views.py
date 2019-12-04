f5c61043eb4e23331db9d0e0b35aae0d39a47a8d
party4bread <party4bread@gmail.com>
2019-12-04 04:25:49 +0000

    Some change for suhang

--- mainsite/views.py
+++ mainsite/views.py
@@ -4,6 +4,8 @@ import json
 import base64
 from .models import RoomLists
 import random
+from django.views.generic.base import View
+
 
 def index(request):
     return render(request, 'mainsite/index.html', {})
@@ -24,26 +26,27 @@ def room(request, room_name):
         'room_name':room_name
     })
 
-def create_room(req):
-    n=random.randint(0,62**3)
-    charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    chs = []
-    while n > 0:
-        r = n % 62
-        n //= 62
-        chs.append(charset[r])
 
-    if len(chs) > 0:
-        chs.reverse()
-    else:
-        chs.append("0")
+class CreateRoomView(View):
+    def get(self, request, *args, **kwargs):
+        n=random.randint(0,62**3)
+        charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+        chs = []
+        while n > 0:
+            r = n % 62
+            n //= 62
+            chs.append(charset[r])
 
-    s = "".join(chs)
-    s = charset[0] * max(4 - len(s), 0) + s
-    RoomLists(room_number=s).save()
-    if not req.session.get('owned_rooms', False):
-        req.session['owned_rooms']=[]
-    req.session['owned_rooms'].append(s)
-    req.session.modified = True
-    print(req.session['owned_rooms'])
-    return redirect(room,room_name=s)
+        if len(chs) > 0:
+            chs.reverse()
+        else:
+            chs.append("0")
+
+        s = "".join(chs)
+        s = charset[0] * max(4 - len(s), 0) + s
+        RoomLists(room_number=s).save()
+        if not request.session.get('owned_rooms', False):
+            request.session['owned_rooms']=[]
+        request.session['owned_rooms'].append(s)
+        request.session.modified = True
+        return redirect(room,room_name=s)
